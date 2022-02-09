1061415260df14f5dfbd5126faa8e28317193462
YAO <kirayao@hotmail.com>
2022-02-09 21:54:52 +0800

    feat: code-run add {{origin}} mark

--- comps/code-run/code-run.js
+++ comps/code-run/code-run.js
@@ -1,14 +1,14 @@
 Component(async ({ load }) => {
-    if (!window.hljs) {
-        // 代码高亮
-        // await load("https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/highlight.min.js").catch(e => {
-        //     return load("../../libs/highlight.min.js");
-        // });
+  if (!window.hljs) {
+    // 代码高亮
+    // await load("https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/highlight.min.js").catch(e => {
+    //     return load("../../libs/highlight.min.js");
+    // });
 
-        await load("../../libs/highlight.min.js");
-    }
+    await load("../../libs/highlight.min.js");
+  }
 
-    $("head").push(`
+  $("head").push(`
     <style>
     @font-face {
         font-family: 'iconfont-codeview';  /* Project id 2748481 */
@@ -16,43 +16,44 @@ Component(async ({ load }) => {
              url('//at.alicdn.com/t/font_2748481_90pq5z3zig9.woff?t=1632027936638') format('woff'),
              url('//at.alicdn.com/t/font_2748481_90pq5z3zig9.ttf?t=1632027936638') format('truetype');
     }
-    </style>`)
+    </style>`);
 
-    // 消除回车后的若干空格
-    // 必须匹配到每一个行的个数
-    function removeSpace(code) {
-        // 先分行
-        let all_line = code.match(/\n.+/g);
+  // 消除回车后的若干空格
+  // 必须匹配到每一个行的个数
+  function removeSpace(code) {
+    // 先分行
+    let all_line = code.match(/\n.+/g);
 
-        if (all_line) {
-            // 去掉空行
-            all_line = all_line.filter(e => e.replace(/\n +/, ""))
+    if (all_line) {
+      // 去掉空行
+      all_line = all_line.filter((e) => e.replace(/\n +/, ""));
 
-            // 匹配空格
-            let all_line_spaces = all_line.map(e => {
-                if (e.match(/\n */)) {
-                    return e.match(/\n */)[0].replace("\n", "");
-                }
-                return e;
-            })
-
-            // 获取最小空格数
-            let min_space_num = all_line_spaces.map(e => e.length).sort((a, b) => a - b)[0];
-
-            if (min_space_num) {
-                // 去除最小数目的换行前置空格
-                let reg = new RegExp(`\\n {${min_space_num}}`, "g");
-                code = code.replace(reg, "\n");
-            }
+      // 匹配空格
+      let all_line_spaces = all_line.map((e) => {
+        if (e.match(/\n */)) {
+          return e.match(/\n */)[0].replace("\n", "");
         }
+        return e;
+      });
 
+      // 获取最小空格数
+      let min_space_num = all_line_spaces
+        .map((e) => e.length)
+        .sort((a, b) => a - b)[0];
 
-        return code;
+      if (min_space_num) {
+        // 去除最小数目的换行前置空格
+        let reg = new RegExp(`\\n {${min_space_num}}`, "g");
+        code = code.replace(reg, "\n");
+      }
     }
 
-    // 生成html文件
-    function toHTMLFile(code, title = 'demo', headCode) {
-        let str = `<!DOCTYPE html>
+    return code;
+  }
+
+  // 生成html文件
+  function toHTMLFile(code, title = "demo", headCode) {
+    let str = `<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -69,98 +70,103 @@ Component(async ({ load }) => {
 
 </html>`;
 
-        // 生成文件
-        let file = new File([str], title + '.html', {
-            type: "text/html",
-        });
+    // 生成文件
+    let file = new File([str], title + ".html", {
+      type: "text/html",
+    });
 
-        return file;
-    }
+    return file;
+  }
 
-    return {
-        attrs: {
-            name: "demo",
-            showCode: null,
-            frameHeight: ""
-        },
-        data: {
-            // head上的代码 
-            headCode: "",
-            // 源代码
-            code: "",
-            // 根据code生成的html文件地址
-            preview_url: "",
-            // 首次输入的code
-            firstCode: ""
-        },
-        watch: {
-            frameHeight(h) {
-                if (h) {
-                    this.shadow.$("#mainFrame").style.height = h + 'px';
-                }
-            },
-            code(code) {
-                if (!code) {
-                    return;
-                }
-
-                code = removeSpace(code);
-
-                if (!this.firstCode) {
-                    this.firstCode = code;
-                }
-
-                this.shadow.$("#content").text = code;
-                hljs.highlightElement(this.shadow.$("code").ele);
-
-                let file = toHTMLFile(code, this.name, this.headCode);
-
-                URL.revokeObjectURL(this.preview_url);
-                this.preview_url = URL.createObjectURL(file);
-
-            }
-        },
-        proto: {
-            get viewShowCode() {
-                return this.showCode !== null;
-            },
-            set viewShowCode(bool) {
-                this.showCode = bool ? "" : null;
-            },
-            reloadView() {
-                let { preview_url } = this;
-
-                this.preview_url = '';
-
-                setTimeout(() => {
-                    this.preview_url = preview_url;
-                }, 100);
-            },
-            openNew() {
-                window.open(this.preview_url);
-            },
-            changeCode(e) {
-                this.code = e.target.textContent;
-            },
-            restoreCode() {
-                this.code = this.firstCode;
-            }
-        },
-        ready() {
-            // 查找内部templte元素
-            if (this[0]) {
-                // 将headcode拿走
-                let codehead = this[0].ele.content.querySelector("codehead");
-                if (codehead) {
-                    codehead.parentNode.removeChild(codehead);
-                    this.headCode = codehead.innerHTML;
-                }
-
-                this.code = removeSpace(this[0].html.trim());
-            }
-        },
-        detached() {
-            URL.revokeObjectURL(this.preview_url);
+  return {
+    attrs: {
+      name: "demo",
+      showCode: null,
+      frameHeight: "",
+    },
+    data: {
+      // head上的代码
+      headCode: "",
+      // 源代码
+      code: "",
+      // 根据code生成的html文件地址
+      preview_url: "",
+      // 首次输入的code
+      firstCode: "",
+    },
+    watch: {
+      frameHeight(h) {
+        if (h) {
+          this.shadow.$("#mainFrame").style.height = h + "px";
         }
-    };
+      },
+      code(code) {
+        if (!code) {
+          return;
+        }
+
+        code = removeSpace(code);
+
+        if (!this.firstCode) {
+          this.firstCode = code;
+        }
+
+        this.shadow.$("#content").text = code;
+        hljs.highlightElement(this.shadow.$("code").ele);
+
+        let file = toHTMLFile(code, this.name, this.headCode);
+
+        URL.revokeObjectURL(this.preview_url);
+        this.preview_url = URL.createObjectURL(file);
+      },
+    },
+    proto: {
+      get viewShowCode() {
+        return this.showCode !== null;
+      },
+      set viewShowCode(bool) {
+        this.showCode = bool ? "" : null;
+      },
+      reloadView() {
+        let { preview_url } = this;
+
+        this.preview_url = "";
+
+        setTimeout(() => {
+          this.preview_url = preview_url;
+        }, 100);
+      },
+      openNew() {
+        window.open(this.preview_url);
+      },
+      changeCode(e) {
+        this.code = e.target.textContent;
+      },
+      restoreCode() {
+        this.code = this.firstCode;
+      },
+    },
+    ready() {
+      // 查找内部templte元素
+      if (this[0]) {
+        // 将headcode拿走
+        let codehead = this[0].ele.content.querySelector("codehead");
+        if (codehead) {
+          codehead.parentNode.removeChild(codehead);
+
+          let chHtml = codehead.innerHTML;
+
+          // 修正 {{origin}}
+          chHtml = chHtml.replace(/\{\{origin\}\}/g, location.origin);
+
+          this.headCode = chHtml;
+        }
+
+        this.code = removeSpace(this[0].html.trim());
+      }
+    },
+    detached() {
+      URL.revokeObjectURL(this.preview_url);
+    },
+  };
 });
--- comps/code-run/readme.md
+++ comps/code-run/readme.md
@@ -1,6 +1,6 @@
 # code-run
 
-方便查看并预览单个html文件的组件；
+方便查看并预览单个 html 文件的组件；
 
 支持修改内部代码、新窗口打开和下载网页功能；
 
@@ -8,7 +8,7 @@
 
 ### 引用资源
 
-#### 方法1：script标签引用
+#### 方法 1：script 标签引用
 
 先将 ofajs 和 code-run 组件引用到网页；（确保 code-run 在 ofajs 后载入）
 
@@ -17,7 +17,7 @@
 <script src="https://cdn.jsdelivr.net/gh/kirakiray/o-book/comps/code-run/code-run.js"></script>
 ```
 
-#### 方法2：load函数引用
+#### 方法 2：load 函数引用
 
 使用 ofajs 模块化系统引用；
 
@@ -27,13 +27,13 @@ load("https://cdn.jsdelivr.net/gh/kirakiray/o-book/comps/code-run/code-run.js");
 
 ### 组件使用
 
-在 `code-run` 标签内使用 `template` 存放需要预览的html内容；
+在 `code-run` 标签内使用 `template` 存放需要预览的 html 内容；
 
 ```html
 <code-run>
-    <template>
-        <div style="color:red;">I am text</div>
-    </template>
+  <template>
+    <div style="color:red;">I am text</div>
+  </template>
 </code-run>
 ```
 
@@ -53,9 +53,9 @@ load("https://cdn.jsdelivr.net/gh/kirakiray/o-book/comps/code-run/code-run.js");
 
 ```html
 <code-run show-code>
-    <template>
-        <div style="color:blue;">I am text</div>
-    </template>
+  <template>
+    <div style="color:blue;">I am text</div>
+  </template>
 </code-run>
 ```
 
@@ -75,13 +75,13 @@ load("https://cdn.jsdelivr.net/gh/kirakiray/o-book/comps/code-run/code-run.js");
 
 ### name
 
-当前预览html的文件名；下载和预览文件内 title；
+当前预览 html 的文件名；下载和预览文件内 title；
 
 ```html
 <code-run name="new demo">
-    <template>
-        <div style="color:green;">I am text</div>
-    </template>
+  <template>
+    <div style="color:green;">I am text</div>
+  </template>
 </code-run>
 ```
 
@@ -93,24 +93,24 @@ load("https://cdn.jsdelivr.net/gh/kirakiray/o-book/comps/code-run/code-run.js");
 
 ## codehead
 
-可添加 `codehead` 标签来引用资源，`codehead`标签的内容会被塞到新建文件的head内，不会显示在代码展示上；
+可添加 `codehead` 标签来引用资源，`codehead`标签的内容会被塞到新建文件的 head 内，不会显示在代码展示上；
 
 ```html
 <code-run name="clickTest" show-code>
-    <template>
-        <codehead>
-            <!-- 新建后会被放到顶部的内容 -->
-            <!-- 不会被展示到代码显示上 -->
-            <script src="https://cdn.jsdelivr.net/gh/kirakiray/ofa.js/dist/ofa.js"></script>
-        </codehead>
-        <div id="consoler">I am consoler</div>
-        <button id="btn">Click Me!</button>
-        <script>
-            $("#btn").on("click", () => {
-                $("#consoler").text = "Hello world!";
-            });
-        </script>
-    </template>
+  <template>
+    <codehead>
+      <!-- 新建后会被放到顶部的内容 -->
+      <!-- 不会被展示到代码显示上 -->
+      <script src="https://cdn.jsdelivr.net/gh/kirakiray/ofa.js/dist/ofa.js"></script>
+    </codehead>
+    <div id="consoler">I am consoler</div>
+    <button id="btn">Click Me!</button>
+    <script>
+      $("#btn").on("click", () => {
+        $("#consoler").text = "Hello world!";
+      });
+    </script>
+  </template>
 </code-run>
 ```
 
@@ -130,3 +130,27 @@ load("https://cdn.jsdelivr.net/gh/kirakiray/o-book/comps/code-run/code-run.js");
         </script>
     </template>
 </code-run>
+
+### {{origin}} 标识
+
+codehead 内可使用 `{{origin}}` 标识替换为运行目录的 `location.origin`
+
+```html
+<code-run name="clickTest" show-code>
+  <template>
+    <codehead>
+      <script src="https://cdn.jsdelivr.net/gh/kirakiray/ofa.js/dist/ofa.js"></script>
+      <!-- 假如当前网页运行在 https://xxxx.com 域名上，
+      {{origin}}/o-book.js 会被替换为当前域名 https://xxxx.com/o-book.js -->
+      <script src="{{origin}}/o-book.js"></script>
+    </codehead>
+    <div id="consoler">I am consoler</div>
+    <button id="btn">Click Me!</button>
+    <script>
+      $("#btn").on("click", () => {
+        $("#consoler").text = "Hello world!";
+      });
+    </script>
+  </template>
+</code-run>
+```
