cc2c054a9c9041f07186fd4dd4f7ad654db9d63d
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-16 12:02:29 +0800

    fix: issuse about loading

---
+++ statics/init.js
@@ -0,0 +1,46 @@
+(() => {
+  if (localStorage.isDark === "true") {
+    $("html").classList.add("dark");
+  } else {
+    $("html").classList.remove("dark");
+  }
+
+  const url = import.meta.url;
+  function resolvePath(path) {
+    return new URL(path, url).href;
+  }
+
+  const load = lm(import.meta);
+
+  Promise.all([
+    load("./comps/article-nav/article-nav.html"),
+    load("./comps/doc-code.html"),
+    load("./comps/doc-header/doc-header.html"),
+    load("./comps/doc-search/doc-search.html"),
+    load("./comps/article-container.html"),
+    load("./comps/article-footer.html"),
+    load("./comps/d-item.html"),
+    load("./comps/d-nav.html"),
+    load("./comps/doc-aside.html"),
+    load("./comps/doc-container.html"),
+    load("./comps/exm-article.html"),
+    load("./comps/lang-select.html"),
+    load("./comps/simp-editor.html"),
+    load("./layouts/article-layout.html"),
+    load("./layouts/header-layout.html"),
+    fetch(resolvePath("./css/github-markdown.css")),
+    fetch(resolvePath("./css/public.css")),
+    fetch(resolvePath("./comps/article-nav/article-nav-host.css")),
+    fetch(resolvePath("./comps/doc-header/doc-header.css")),
+  ]).then(() => {
+    const loading = $("#loading");
+    if (loading) {
+      setTimeout(() => {
+        loading.classList.add("fadeout");
+        setTimeout(() => {
+          loading.remove();
+        }, 400);
+      }, 200);
+    }
+  });
+})();
