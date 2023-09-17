45cf591dc42b6a4ff4d880804001fcd50e17d4e5
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-17 23:13:48 +0800

    fix: link problem

--- statics/page-init.mjs
+++ statics/page-init.mjs
@@ -27,7 +27,7 @@ export default async (PATH, [{ query, load }]) => {
       this.shadow.all("article a").forEach((e) => {
         const href = e.attr("href") || "";
 
-        if (href.includes(location.origin)) {
+        if (href.includes(location.origin) && /\.html/.test(href)) {
           e.attr("olink", "");
         }
       });
