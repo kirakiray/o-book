d07e3d06c2f97d687ee2b307203875b5393ad3ff
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-18 16:09:50 +0800

    chore: bump version

--- docs/doc-sw.js
+++ docs/doc-sw.js
@@ -1,2 +1,2 @@
-// importScripts(`https://cdn.jsdelivr.net/npm/obook@2.1.1/src/sw/base.js`);
+// importScripts(`https://cdn.jsdelivr.net/npm/obook@2.1.2/src/sw/base.js`);
 importScripts(`../src/sw/base.js`);
