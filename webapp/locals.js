ec523d0eac25d3641f9e47ce27f0ebe73ef43a3a
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-19 18:03:36 +0800

    feat: ok the preview app

---
+++ webapp/locals.js
@@ -0,0 +1,29 @@
+const cn = {
+  notAccess: "抱歉，您当前使用的浏览器不支持文件访问 API。",
+  desc1: "创建您的文档站",
+  desc2:
+    "请放心，这个应用没有后端服务器，不会上传您的内容。它仅在浏览器端生成您的文档站。",
+  selectBtn: "选择本地文件夹",
+  initDesc: "当前目录未找到与文档相关的基础文件。是否要进行初始化？",
+  confirmInitDesc: "确认进行初始化",
+  initing: "正在写入项目文件...",
+  initing2: "项目文件写入完成，正在进行初始化",
+  donotClose: `<b>请不要关闭此标签</b>，点击下方链接进行预览。<p>请打开相应的文件夹以开始编写您的文档。</p>`,
+};
+
+const en = {
+  notAccess:
+    "Sorry, your current browser does not support the file access API.",
+  desc1: "Create your document site",
+  desc2:
+    "Rest assured, this application has no backend server and will not upload your content. It only generates your document site on the client-side in your browser.",
+  selectBtn: "Select Local Folder",
+  initDesc:
+    "No basic document-related files were found in the current directory. Would you like to initialize?",
+  confirmInitDesc: "Confirm Initialization",
+  initing: "Writing project files...",
+  initing2: "Project files have been written, initialization in progress",
+  donotClose: `<b>Please do not close this tab</b>. Click the link below to preview.<p>Please open the corresponding folder to start writing your document.</p>`,
+};
+
+export { cn, en };
