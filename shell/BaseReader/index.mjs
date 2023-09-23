cfee13720ebfe2b585448d1551ad98abdfd5c869
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-23 16:56:34 +0800

    feat: add base reader

---
+++ shell/BaseReader/index.mjs
@@ -0,0 +1,17 @@
+import BaseReader from "./base.mjs";
+
+export { BaseReader };
+
+// 底部兼容层，方便以后迁移到浏览器
+export async function readDir(path) {
+  return new BaseReader(path);
+}
+
+export async function write(path, content) {
+  const dirpath = path.replace(/(.+)\/(.+)/, "$1");
+  const name = path.replace(/(.+)\/(.+)/, "$2");
+
+  const reader = new BaseReader(dirpath, true);
+
+  return await reader.write(name, content);
+}
--- shell/index.js
+++ shell/index.js
@@ -13,12 +13,22 @@ if (packagejson) {
   obook = packagejson.obook;
 }
 
-if (process.argv.includes("update")) {
+const { argv } = process;
+
+if (argv.includes("update")) {
   // update
   update({ obook });
-} else if (process.argv.includes("init")) {
+} else if (argv.includes("init")) {
   // init
   init({ obook });
-} else {
+} else if (argv.includes("dev") || argv.includes("build")) {
   dev({ obook });
+} else {
+  console.log(`
+Please select the command you want to execute:
+
+dev: Enter development mode, you can preview the obook packaged file in real time.
+build: Package the project, make sure the obook property is set in the package.json file.
+update: Upgrade all obook in the project to the latest version.
+`);
 }
