24c88a1c9f5edf9c24931a622cb082056adbd268
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-10-09 19:08:16 +0800

    chore: bump version

--- src/sw/base.js
+++ src/sw/base.js
@@ -3,10 +3,10 @@
 // if (hostUrlData.hostname.includes("127")) {
 //   host = hostUrlData.origin;
 // } else {
-//   host = "https://cdn.jsdelivr.net/npm/obook@2.1.23";
+//   host = "https://cdn.jsdelivr.net/npm/obook@2.1.24";
 // }
 
-const host = "https://cdn.jsdelivr.net/npm/obook@2.1.23";
+const host = "https://cdn.jsdelivr.net/npm/obook@2.1.24";
 
 importScripts(
   "https://cdn.jsdelivr.net/npm/marked/marked.min.js",
