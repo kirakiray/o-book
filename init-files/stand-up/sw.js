9339cdc65946c621d86990ca9ab7878de74490b9
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-10-13 18:25:39 +0800

    chore: bump version

--- init-files/stand-up/sw.js
+++ init-files/stand-up/sw.js
@@ -1,1 +1,1 @@
-importScripts(`https://cdn.jsdelivr.net/npm/obook@2.1.26/src/sw/base.js`);
+importScripts(`https://cdn.jsdelivr.net/npm/obook@2.1.27/src/sw/base.js`);
--- src/sw/base.js
+++ src/sw/base.js
@@ -3,12 +3,12 @@
 // if (hostUrlData.hostname.includes("127")) {
 //   host = hostUrlData.origin;
 // } else {
-//   host = "https://cdn.jsdelivr.net/npm/obook@2.1.26";
+//   host = "https://cdn.jsdelivr.net/npm/obook@2.1.27";
 // }
 
 // const host = "http://127.0.0.1:5512";
 
-const host = "https://cdn.jsdelivr.net/npm/obook@2.1.26";
+const host = "https://cdn.jsdelivr.net/npm/obook@2.1.27";
 
 importScripts(
   "https://cdn.jsdelivr.net/npm/marked/marked.min.js",
