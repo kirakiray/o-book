113595deef1137b8474f293c18a738555cbcaf5f
Yao <kirayao@hotmail.com>
2023-10-13 18:28:45 +0800

    feat: Optimize multi-language experience and sitemap generation (#35)
    * feat: Add a prompt to select a language
    
    * fix: Redirection page can be jumped according to the system language
    
    * feat: Can generate sitemap.xml
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- docs/doc-sw.js
+++ docs/doc-sw.js
@@ -1,5 +1,5 @@
 if (serviceWorker.scriptURL.includes("127")) {
   importScripts(`../src/sw/base.js`);
 } else {
-  importScripts(`https://cdn.jsdelivr.net/npm/obook@2.1.26/src/sw/base.js`);
+  importScripts(`https://cdn.jsdelivr.net/npm/obook@2.1.27/src/sw/base.js`);
 }
--- docs/t-cn/index.md
+++ docs/t-cn/index.md
@@ -1,6 +1,6 @@
 # obook：最簡單的文檔建站工具
 
-<l-m src="https://cdn.jsdelivr.net/npm/obook@2.1.26/blocks/simp-block.html"></l-m>
+<l-m src="https://cdn.jsdelivr.net/npm/obook@2.1.27/blocks/simp-block.html"></l-m>
 
 <simp-block>
 
