dc01485bbdaf1836fec526db510d4d3c1771cc5a
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-10-17 15:50:27 +0800

    refactor: remove useless code

--- src/sw/base.js
+++ src/sw/base.js
@@ -3,12 +3,12 @@
 // if (hostUrlData.hostname.includes("127")) {
 //   host = hostUrlData.origin;
 // } else {
-//   host = "https://cdn.jsdelivr.net/npm/obook@2.1.29";
+//   host = "https://cdn.jsdelivr.net/npm/obook@2.1.30";
 // }
 
 // const host = "http://127.0.0.1:5512";
 
-const host = "https://cdn.jsdelivr.net/npm/obook@2.1.29";
+const host = "https://cdn.jsdelivr.net/npm/obook@2.1.30";
 
 importScripts(
   "https://cdn.jsdelivr.net/npm/marked/marked.min.js",
