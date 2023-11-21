8ea0e65e363e221412a1368e45ee79cbd7379477
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-11-21 18:54:01 +0800

    backup

--- src/sw/base.js
+++ src/sw/base.js
@@ -6,9 +6,9 @@
 //   host = "https://cdn.jsdelivr.net/npm/obook@2.1.41";
 // }
 
-// const host = "http://127.0.0.1:5512";
+const host = "http://127.0.0.1:5512";
 
-const host = "https://cdn.jsdelivr.net/npm/obook@2.1.41";
+// const host = "https://cdn.jsdelivr.net/npm/obook@2.1.41";
 
 importScripts(
   "https://cdn.jsdelivr.net/npm/marked/marked.min.js",
