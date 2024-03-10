265b97109d04ffc1fdc104ea2d6a865cdd117602
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-03-10 14:30:57 +0800

    fix: markdown style

--- demos/sample/cn/test2/sub/article-one.md
+++ demos/sample/cn/test2/sub/article-one.md
@@ -1,3 +1,5 @@
 # Article One
 
 I am Article One
+
+[to article test](../../test1/article-test.md)
--- statics/data.js
+++ statics/data.js
@@ -32,6 +32,8 @@ export const articlesList = $.stanz([
   },
 ]);
 
+const gloData = {};
+
 if (typeof configUrl === "string") {
   // 加载数据
   (async () => {
@@ -39,14 +41,47 @@ if (typeof configUrl === "string") {
       return e.json();
     });
 
+    // 初始化顶部
     navs.splice(0, 1000, ...configData.navs);
+
+    Object.assign(gloData, { articleData, configData });
 
     console.log(articleData, configData);
   })();
 }
 
-export const resetData = (path) => {
-  console.log("resetData", path);
+export const initPath = (path) => {
+  const {
+    configData: { navs },
+    articleData: { childs: dirs },
+  } = gloData;
+
+  console.log("initPath", path);
+
+  // 修正左侧导航的内容
+  // 先判断在哪个导航上，修正为那个导航上的内容
+  const targetNav = navs.find((e) => {
+    const reg = new RegExp("^" + e.src + "/");
+    if (reg.test(path)) {
+      return true;
+    }
+  });
+
+  if (targetNav) {
+    // 存在导航地址内，修正顶部导航的激活
+    showLeft.value = true;
+
+    const firstDirName = targetNav.src.split("/")[0];
+
+    const targetDir = dirs.find((e) => e.name === firstDirName);
+
+    if (targetDir) {
+      articlesList.splice(0, 1000, ...targetDir.childs);
+    }
+  } else {
+    // 不在导航内，隐藏左侧菜单
+    showLeft.value = false;
+  }
 };
 
 window.showLeft = showLeft;
