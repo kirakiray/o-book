d58ac97e093a4562054210e63373260e0b08a2c8
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-03-21 15:11:29 +0800

    feat: add static-server

---
+++ scripts/build.js
@@ -0,0 +1,19 @@
+import { createWriteStream } from "fs";
+import archiver from "archiver";
+
+async function zipDirectory(source, out) {
+  const archive = archiver("zip", { zlib: { level: 9 } });
+  const stream = createWriteStream(out);
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(source, false)
+      .on("error", (err) => reject(err))
+      .pipe(stream);
+
+    stream.on("close", () => resolve());
+    archive.finalize();
+  });
+}
+
+zipDirectory("./statics", "./_statics.zip");
