00605f732c791c7deca225dd9880020ed71bdd0e
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-04-06 15:34:44 +0800

    refactor: backup code

--- src/translate.js
+++ src/translate.js
@@ -4,15 +4,24 @@ export const translate = async ({ content, callback }) => {
   const data = {
     model: "gemma:7b",
     // prompt: "你会翻译什么人类的语言?",
-    prompt: `将下面的内容翻译成英文：\n ${content}`,
+    // prompt: `将下面的内容翻译成英文，不用返回多余的内容：
+    // ${content}`,
+    // prompt: `<start_of_turn>user
+    // You are a helpful 2nd-grade teacher. Help a 2nd grader to answer questions in a short and clear manner.
+    // Explain why the sky is blue<end_of_turn>
+    // <start_of_turn>model`,
+    prompt: `<start_of_turn>user
+    Translate the content into Spanish: 
+    ${content}<end_of_turn>
+    <start_of_turn>model`,
   };
 
   const response = await fetch(generateUrl, {
     method: "POST",
     body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
   });
 
   const reader = response.body.getReader();
@@ -28,7 +37,29 @@ export const translate = async ({ content, callback }) => {
         return;
       }
 
-      const data = JSON.parse(new TextDecoder("utf-8").decode(value));
+      const text = new TextDecoder("utf-8").decode(value);
+      let data;
+      try {
+        data = JSON.parse(text);
+      } catch (err) {
+        console.log("error", text);
+
+        const newResponseData = {
+          response: "",
+          done: false,
+        };
+
+        text.split(/\n/g).forEach((newText) => {
+          const newData = JSON.parse(newText);
+          newResponseData = {
+            ...newResponseData,
+            ...newData,
+            response: newResponseData.response + newData.response,
+          };
+        });
+
+        data = newResponseData;
+      }
 
       //   console.log("data: ", data);
 
