5a28ea1c1665acfab4e7f92d6993834d1bee4941
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-10-31 22:56:21 +0800

    chore: bump version

--- statics/init.js
+++ statics/init.js
@@ -7,7 +7,7 @@
 
   lm.config({
     alias: {
-      "@obook": "https://cdn.jsdelivr.net/npm/obook@2.1.43",
+      "@obook": "https://cdn.jsdelivr.net/npm/obook@2.1.44",
       // "@obook": "http://127.0.0.1:5512",
     },
   });
